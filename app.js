75c32f81ba63edc766c3ca17f6bca4a7b3df8846
tegrisko <tegrisko@outlook.com>
2019-12-06 14:32:51 +0700

    toglogchiin eeljiig solidog bolob

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // toglogchiin eeljiig hadgalah var 1-eer toglogchiig 0,  2-aar toglogchiig 1 gey
 
-var activePlayer=1;
+var activePlayer=0;
 
 // toglogchiin stugluulsan onoog hadgalah var
 
@@ -16,26 +16,51 @@ var diceNumber=Math.floor(Math.random()*6)+1;
 // window.document.querySelector("#score-0").textContent=dice
 // window.document.querySelector("#score-1").textContent=dice
 
-document.getElementById("score-1").textContent='0';
+
 
 document.getElementById("score-0").textContent='0';
 
-document.getElementById("current-1").textContent='0';
+document.getElementById("score-1").textContent='0';
 
 document.getElementById("current-0").textContent='0';
+
+document.getElementById("current-1").textContent='0';
 
 var diceDom= document.querySelector(".dice")
 
 diceDom.style.display='none';
 
+// shoog shideh eventListener
 document.querySelector(".btn-roll").addEventListener("click", function(){
 
+    // shoonii zurgiig gargah
     diceDom.style.display='block';
 
+    // 1-6 dotorh sanamsargui toog gargah
     var diceNumber=Math.floor(Math.random()*6)+1;
 
+    // diceNumber d hargalzah zurgiig uzuuleh
     diceDom.src ="dice-"+ diceNumber +".png";
 
-    
+    // buusan too 1ees yalgaatai ued activePlayer iin score iig nemegduulne
+    if(diceNumber !==1){
+        roundScore=roundScore+diceNumber
+
+        document.getElementById("current-"+activePlayer).textContent=roundScore;
+    }else{
+        roundScore=0;
+
+        document.getElementById("current-"+activePlayer).textContent=0;
+
+        activePlayer===0 ? (activePlayer=1) : (activePlayer=0);
+
+        document.querySelector(".player-0-panel").classList.toggle("active");
+
+        document.querySelector(".player-1-panel").classList.toggle("active");
+
+        diceDom.style.display="none";
+    }
 });
+
+
 
